2bd35f58fe747d087bd68f7d4cfe96cc0053d12f
Ing. Rosdanys Ramirez Arcas <rosdanys@dgtalliance.com>
2022-11-12 01:35:00 -0500

    feat: add sentry

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,22 @@ import { store } from './store'
 
 import './context/i18n'
 
+
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
+
+ Sentry.init({
+  dsn: "https://321bd1acb87940a49068ee8e7533d020@o176772.ingest.sentry.io/6396526",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+ 
+
+
 ReactDOM.createRoot(
   document.getElementById('ib-wrapper-quick-search-rentals')
 ).render(
